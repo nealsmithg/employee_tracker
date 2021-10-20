b27a94e0ce673582fab40d891530c21b8fe9119d
Cornelius Smith <nealsmithg@gmail.com>
2021-10-20 19:04:32 -0400

    first draft of schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+drop database if exists cms_db;
+create database cms_db;
+
+use cms_db:
+
+create table department (
+    id int not null auto_increment primary key,
+    name varchar(30 )not null
+);
+
+create table role (
+    id int not null auto_increment primary key,
+    title varchar(30) not null,
+    salary decimal not null,
+    dapartment_id int,
+    foreign key (dapartment_id)
+    references department (id)
+    on delete set null
+);
+
+create table employee (
+    id int not null auto_increment primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+    foreign key (role_id)
+    references role(id)
+    on delete set null,
+    foreign key (manager_id)
+    references table(id)
+)
